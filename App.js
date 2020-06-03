fe60f7939f32a7674aa0817c5b1542d619cb7c56
TAKAHARA <>
2020-06-03 13:15:08 +0900

    add FlatList

--- App.js
+++ App.js
@@ -1,67 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, SafeAreaView, FlatList } from 'react-native';
+
+// json
+import dummyArticles from './dummies/articles';
+
+// components
+import ListItem from './src/components/ListItem';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  itemContainer: {
-    height: 96,
-    width: '100%',
-    borderColor: '#ccc',
-    borderBottomWidth: 1,
-    flexDirection: 'row',
-  },
-  leftContainer: {
-    flex: 1,
-    padding: 12,
-    justifyContent: 'space-between',
-  },
-  rightContainer: {
-    width: 96,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  thumbnail: {
-    width: 80,
-    height: 80,
-    borderRadius: 12,
-  },
-  title: {
-    fontSize: 16,
-  },
-  publisher: {
-    fontSize: 12,
-    color: '#9E9E9E',
   },
 });
 
 export default function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setArticles(dummyArticles);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}>
-          <Text style={styles.title} numberOfLines={3}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Text>
-          <Text style={styles.publisher}>ReactNews</Text>
-        </View>
-        <View style={styles.rightContainer}>
-          <Image
-            style={styles.thumbnail}
-            source={{ uri: 'https://picsum.photos/id/10/200/200' }}
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
           />
-        </View>
-      </View>
-    </View>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
   );
 }
---
+++ src/components/ListItem.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    height: 96,
+    width: '100%',
+    borderColor: '#ccc',
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    flex: 1,
+    padding: 12,
+    justifyContent: 'space-between',
+  },
+  rightContainer: {
+    width: 96,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  thumbnail: {
+    width: 80,
+    height: 80,
+    borderRadius: 12,
+  },
+  title: {
+    fontSize: 16,
+  },
+  author: {
+    fontSize: 12,
+    color: '#9E9E9E',
+  },
+});
+
+const ListItem = ({ imageUrl, title, author }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.leftContainer}>
+        <Text style={styles.title} numberOfLines={3}>
+          {title}
+        </Text>
+        <Text style={styles.author}>{author}</Text>
+      </View>
+      <View style={styles.rightContainer}>
+        <Image style={styles.thumbnail} source={{ uri: imageUrl }} />
+      </View>
+    </View>
+  );
+};
+
+export default ListItem;
